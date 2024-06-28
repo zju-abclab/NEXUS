f036a204f01e5c58e3fcfb151b9d2c0a4adb9895
Kevin-Zh-CS <sdaqzjw@126.com>
2024-06-28 17:34:03 +0800

    modify files

--- src/bootstrapping/common/cmake_install.cmake
+++ src/bootstrapping/common/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zhangjiawen/Code/secureGPT2/cpu-ckks/single-key/ckks_bootstrapping
+# Install script for directory: /home/zhangjiawen/Code/NEXUS/src/bootstrapping/common
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
